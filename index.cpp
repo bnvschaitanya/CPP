776758747ea480953b3f86d96ee5aea04f4b2ed3
B N V S Chaitanya <bellamkonda.nvschaitanya@gmail.com>
2025-11-01 13:27:54 +0530

    second commit

--- index.cpp
+++ index.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int main(){
-    cout<<"Hello World !!, Starting the Revision";
+    int a,b;
+    cout<<"Enter a Value:";
+    cin>>a;
+    cout<<"Enter b value:"; 
+    cin>>b;
+    cout<<"Enter Values are "<<a<<" "<<b<<endl;
+    swap(a,b);
+    cout<<"After Swap entered values of a and b are "<<a<<" "<<b<<endl;
     return 0;
 }
